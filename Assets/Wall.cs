e693a9d969319673dedb48d4d0b62284c6715255
shoutatadano <88645579+shoutatadano@users.noreply.github.com>
2021-09-06 16:27:45 +0900

    ファイルの追加

---
+++ Assets/Wall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+
+        if (other.gameObject.tag == "CarTag")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "TrafficConeTag")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "CoinTag")
+        {
+            Destroy(other.gameObject);
+        }
+        
+
+    }
+    
+
+}
